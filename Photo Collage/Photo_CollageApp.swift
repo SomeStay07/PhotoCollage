d36fe718267d22ba44d906c2ed05fe6d66bc3aa3
Тимур Чеберда <cheberda@tochkak.ru>
2021-06-06 16:18:36 +0300

    Initial Commit

---
+++ Photo Collage/Photo_CollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_CollageApp.swift
+//  Photo Collage
+//
+//  Created by Тимур Чеберда on 06.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Photo_CollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
