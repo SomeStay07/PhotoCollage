17df97fc7700a1de8bf4b06174c6137afe2358ca
Тимур Чеберда <cheberda@tochkak.ru>
2021-06-06 20:03:46 +0300

    first commit

---
+++ Photo Collage/ViewState.swift
@@ -0,0 +1,20 @@
+//
+//  ViewState.swift
+//  Photo Collage
+//
+//  Created by Тимур Чеберда on 06.06.2021.
+//
+
+import SwiftUI
+
+struct ViewState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewState_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewState()
+    }
+}
